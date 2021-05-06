fb76e7fa7e2e31ccb01725973a42e050369b21b9
Acmquita <acmesquita19@gmail.com>
2021-05-05 22:02:03 -0300

    Add-functions

--- ex035.py
+++ ex035.py
@@ -4,20 +4,38 @@ def cabecalho():
 	print(' '*5,'SIMULADOR DE EMPRÉSTIMO', ' '*5)
 	print('*' * 35)
 
-cabecalho()
 
-salario = float(input('Qual a renda do comprador? '))
-percent = salario*(30/100)
-print('A parcela máxima que pode ser paga pelo comprador é R${}'.format(percent))
+def valoremprestimo():
+	emprestimo = float(input(f'Qual o valor que vai ser emprestado? Máximo é {valor}'))
+	while emprestimo > valor:
+		emprestimo = float(input('Valor maior que a margem do cliente. Tente um valor mais baixo.'))
+	return emprestimo
 
-valor = 72 * percent
-qtd_prestacao = ceil(valor / percent) # calculo da quantidade prestação , considerando o valor máximo da prestação
-print(f'Com essa renda de {salario} o comprador poderá tomar no máximo {valor} emprestado. Com o prazo máximo de {qtd_prestacao} meses')
+def confirm():
+	confirmation = str(input(f'O valor que deseja emprestar é R${emprestimo}? [S] para Sim, [N] para Não:')).lower().strip()[0]
+	while confirmation not in 'sn':
+		confirmation = str(input('Opção Inválida! [S] para Sim, [N] para Não: '))
 
-emprestimo = float(input(f'Qual o valor que vai ser emprestado? Máximo é {valor}'))
-while emprestimo > valor:
-	emprestimo = float(input('Valor maior que a margem do cliente. Tente um valor mais baixo.'))
+def calculo():
+	salario = float(input('Qual a renda do comprador? '))
+	global percent 
+	percent = salario*(30/100)
+	print('A parcela máxima que pode ser paga pelo comprador é R${}'.format(percent))
 
-confirmation = str(input(f'O valor que deseja emprestar é R${emprestimo}? [S] para Sim, [N] para Não:')).lower()
-while confirmation not in 'sn':
-	confirmation = str(input('Opção Inválida! [S] para Sim, [N] para Não: '))
+	valor = 72 * percent
+	qtd_prestacao = ceil(valor / percent) # calculo da quantidade prestação , considerando o valor máximo da prestação
+	print(f'Com essa renda de {salario} o comprador poderá tomar no máximo {valor} emprestado. Com o prazo máximo de {qtd_prestacao} meses')
+	return valor
+
+def confirmPrazo():
+	for parc in range(24, 72, 6):
+			print(f'{parc}x{emprestimo/parc:.2f}')
+
+if __name__ == '__main__':
+	cabecalho()
+	valor = calculo()
+	emprestimo = valoremprestimo()
+	confirm()
+	confirmPrazo()
+
+	
