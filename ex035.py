727525235bc80be046e1b80d1e044f89f3f781c4
Acmquita <acmesquita19@gmail.com>
2021-05-05 21:20:37 -0300

    Improvements in system

--- ex035.py
+++ ex035.py
@@ -1,35 +1,23 @@
 from math import ceil
-print('*' * 35)
-print(' '*5,'SIMULADOR DE EMPRÉSTIMO', ' '*5)
-print('*' * 35)
-salario = float(input('Qual a renda do comprador? ')) 
+def cabecalho():
+	print('*' * 35)
+	print(' '*5,'SIMULADOR DE EMPRÉSTIMO', ' '*5)
+	print('*' * 35)
+
+cabecalho()
+
+salario = float(input('Qual a renda do comprador? '))
 percent = salario*(30/100)
 print('A parcela máxima que pode ser paga pelo comprador é R${}'.format(percent))
 
+valor = 72 * percent
+qtd_prestacao = ceil(valor / percent) # calculo da quantidade prestação , considerando o valor máximo da prestação
+print(f'Com essa renda de {salario} o comprador poderá tomar no máximo {valor} emprestado. Com o prazo máximo de {qtd_prestacao} meses')
 
-valor = float(input('Qual o valor do imóvel? '))
-prestação = ceil(valor / percent)
-print(prestação)
-parcela = valor/prestação
-if prestação > 30:
-	valor = valor + valor*5/1000
-#for i in range(prestação, 80):
-	#parcela = valor/i
-while parcela > valor/80:
-	parcela = valor/prestação
-	print('{} parcelas de {:.2f}'. format(prestação, parcela))
-	#parcela = parcela + (parcela*5/1000)
-	prestação=prestação+1
+emprestimo = float(input(f'Qual o valor que vai ser emprestado? Máximo é {valor}'))
+while emprestimo > valor:
+	emprestimo = float(input('Valor maior que a margem do cliente. Tente um valor mais baixo.'))
 
-
-
-
-
-
-
-#parcela = valor/prazo
-
-#if parcela > percent:
-#	print('O empréstimo foi negado, pois a parcela é de R$ {} e a margem do comprador é de R$ {:.2f}. '.format(parcela, percent))
-#elif percent >= parcela:
-#	print('O empréstimo foi aprovado! A parcela ficará de R$ {:.2f}, durante {} meses'.format(parcela, prazo))	
+confirmation = str(input(f'O valor que deseja emprestar é R${emprestimo}? [S] para Sim, [N] para Não:')).lower()
+while confirmation not in 'sn':
+	confirmation = str(input('Opção Inválida! [S] para Sim, [N] para Não: '))
